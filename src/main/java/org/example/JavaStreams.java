ffc58210c89e7b35aa3d4f0a7ab043a9cdbc57f5
Gogul k <gogul.k@cormsquare.com>
2024-03-31 10:46:49 +0530

    Java Streams

--- src/main/java/org/example/JavaStreams.java
+++ src/main/java/org/example/JavaStreams.java
@@ -12,22 +12,22 @@ import static org.junit.Assert.assertTrue;
 
 public class JavaStreams {
 
-    static void NormalFirsLetter(){
+    static void NormalFirsLetter() {
         List<String> names = new ArrayList<String>();
         names.add("Aravind");
         names.add("Deebhan");
         names.add("Alexa");
         names.add("Rama");
         names.add("Adam");
-int count = 0;
-        for (int i =0;i<names.size();i++){
+        int count = 0;
+        for (int i = 0; i < names.size(); i++) {
             String name = names.get(i);
-            if (name.startsWith("A")){
+            if (name.startsWith("A")) {
                 count++;
             }
         }
         System.out.println(count);
-    }
+
 //    static void FirstLetter(){
 //        List<String> names = new ArrayList<>();
 //        names.add("Aravind");
@@ -39,7 +39,7 @@ int count = 0;
 //        Long c =names.stream().filter(s->s.startsWith("A")).count();
 //        System.out.println(c);
         // Count of the List
-//        Long d = Stream.of("Aravind", "Deebhan", "Alexa", "Adam", "Rama").filter(s->s.length()>4).count();
+//        Long d = Stream.of("Aravind", "Deebhan", "Alexa", "Adam", "Rama").filter(s -> s.length() > 4).count();
 //        System.out.println(d);
         //Name starts with "A" name list
 //        names.stream().filter(g->g.startsWith("A")).forEach(g->System.out.println(g));
@@ -49,8 +49,8 @@ int count = 0;
 //        names.stream().filter(g->g.endsWith("a")).sorted().forEach(g->System.out.println(g));
         //Greater than 4 letters and Print first name
 //        names.stream().filter(g->g.length()>4).limit(1).forEach(g->System.out.println(g));
-        //The Letters Ends with "a" and it should display in Upper case
-//        names.stream().filter(g->g.endsWith("a")).sorted().map(g->g.toUpperCase()).forEach(g->System.out.println(g));
+//        The Letters Ends with "a" and it should display in Upper case
+        names.stream().filter(g->g.endsWith("a")).sorted().map(g->g.toUpperCase()).forEach(g->System.out.println(g));
         //Combain the Two Array of strings in the single variable
 //        Stream<String>
         //Get the First Elements of the List
@@ -59,6 +59,7 @@ int count = 0;
 //        System.out.println("NA");
 //        System.out.println("NA");
     }
+}
 
 
 
